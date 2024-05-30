ad32ee9776cb9bcd1709a20098dbdb801c4d2e9e
Daffa <daffamfzn@gmail.com>
2024-05-30 22:14:31 +0700

    feat: initialized web server

---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+
+(async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server start at: ${server.info.uri}`);
+})();
