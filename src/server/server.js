a4526a816f8f07ed6502bf994c37f81ec20d9127
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-06-08 01:40:41 +0700

    feat: add load model and config

--- src/server/server.js
+++ src/server/server.js
@@ -15,7 +15,52 @@ const routes = require("../routes");
     },
   });
 
+  const model = await loadModel();
+  server.app.model = model;
+
   server.route(routes);
+
+  server.ext("onPreResponse", function (request, h) {
+    const response = request.response;
+
+    if (response instanceof InputError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: `${response.message} Silakan gunakan foto lain.`,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    if (response.isBoom) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.output.statusCode);
+      return newResponse;
+    }
+
+    if (response.statusCode == 400) {
+      const newResponse = h.response({
+        status: "fail",
+        message: "Terjadi kesalahan dalam melakukan prediksi",
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    if (response.statusCode == 413) {
+      const newResponse = h.response({
+        status: "fail",
+        message: "Payload content length greater than maximum allowed: 1000000",
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server start at: ${server.info.uri}`);
