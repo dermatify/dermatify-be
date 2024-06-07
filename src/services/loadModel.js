f5caa82a75e6bf00cdef8e9cdd53f6e235855ac8
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-06-08 01:52:23 +0700

    feat: add try catch when loading model

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,14 @@
 const tf = require("@tensorflow/tfjs-node");
 async function loadModel() {
-  return tf.loadGraphModel(process.env.MODEL_URL);
+  try {
+    console.log("Model URL:", process.env.MODEL_URL);
+    const model = await tf.loadGraphModel(process.env.MODEL_URL);
+    console.log("Model loaded successfully");
+    return model;
+  } catch (error) {
+    console.error("Error loading the model:", error);
+    return null;
+  }
 }
+
 module.exports = loadModel;
